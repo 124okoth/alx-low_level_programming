be141b1a0e453223052d35fffa92a0f0c445117b
124okoth <124okoth@gmail.com>
2023-08-09 10:15:30 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,10 +1,8 @@
 #include "main.h"
-/**
- * main - Main Entry of program
- * Return: Always (0)
- */
+
 int main(void)
 {
-	_puts("\"my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
+
