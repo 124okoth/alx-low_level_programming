5977099752321ef142e2af8f61891df9e044032a
124okoth <124okoth@gmail.com>
2023-09-28 07:07:34 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion(char *s) - prints a new line string, followed by new line
+ * @s - input
+ * Return - Always 0
+ */
+_puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+
+}
